7e4fa2a41a75a5e8e293a5ef9619dac77d463748
JarvisStark <mrjarviscraft@gmail.com>
2020-01-01 02:11:56 +0300

    Fix List

--- List.h
+++ List.h
@@ -20,7 +20,7 @@ class List {
         friend class List;
 
         const T value_;
-        Node *next_ = nullptr;
+        Node *next_ = nullptr, *previous_ = nullptr;
 
         explicit Node(T value) : value_(value) {}
     };
@@ -28,6 +28,95 @@ class List {
     size_t size_ = 0;
     Node *first_ = nullptr;
     Node *last_ = nullptr;
+
+    void push_only(Node *node) {
+        first_ = last_ = node;
+
+        ++size_;
+    }
+
+    void push_first(Node *node) {
+        node->next_ = first_;
+        first_->previous_ = node;
+        first_ = node;
+
+        ++size_;
+    }
+
+    void push_last(Node *node) {
+        node->previous_ = last_;
+        last_->next_ = node;
+        last_ = node;
+
+        ++size_;
+    }
+
+    void push_after(Node *previous, Node *node) {
+        Node *next = previous->next_;
+
+        previous->next_ = node;
+        node->previous_ = previous;
+
+        next->previous_ = node;
+        node->next_ = next;
+
+        ++size_;
+    }
+
+    void push_before(Node *next, Node *node) {
+        Node *previous = next->previous_;
+
+        previous->next_ = node;
+        node->previous_ = previous;
+
+        next->previous_ = node;
+        node->next_ = next;
+
+        ++size_;
+    }
+
+    T pop_only() {
+        Node *node = first_;
+        T value = node->value_;
+        delete node;
+
+        first_ = last_ = nullptr;
+        size_ = 0;
+
+        return value;
+    }
+
+    T pop_first() {
+        Node *node = first_;
+        (first_ = node->next_)->previous_ = nullptr;
+
+        T value = node->value_;
+        delete node;
+        --size_;
+
+        return value;
+    }
+
+    T pop_last() {
+        Node *node = last_;
+        (last_ = node->previous_)->next_ = nullptr;
+
+        T value = node->value_;
+        delete node;
+        --size_;
+
+        return value;
+    }
+
+    T pop(Node *node) {
+        node->next_->previous_ = node->previous_;
+        node->previous_->next_ = node->next_;
+
+        T value = node->value_;
+        delete node;
+
+        return value;
+    }
 
 public:
     ~List() {
@@ -40,98 +129,102 @@ public:
     }
 
     void push_begin(T value) {
-        Node *node = new Node(value);
-        if (first_ == nullptr) {
-            first_ = last_ = node;
-        } else {
-            node->next_ = first_;
-            first_ = node;
-        }
-        size_++;
+        if (size_ == 0) push_only(new Node(value));
+        else push_first(new Node(value));
     }
 
     void push_end(T value) {
-        Node *node = new Node(value);
-        if (first_ == nullptr) {
-            first_ = last_ = node;
-        } else {
-            Node *new_last = new Node(value);
-            new_last->next_= nullptr;
-            last_->next_=new_last;
-            last_=new_last;
+        if (size_ == 0) push_only(new Node(value));
+        else push_last(new Node(value));
+    }
+
+    void push_i_index(T value, int index) {
+        if (index < 0 || index > size_) throw out_of_range("Bad index");
+
+        if (index == 0) push_first(new Node(value));
+        else if (index == size_) push_last(new Node(value));
+        else {
+            int i = 0;
+            Node *next = first_;
+            while (i++ < index) next = next->next_;
+            push_before(next, new Node(value));
         }
-        size_++;
     }
 
-    void push_i_index(T value, int k) {
-        if (k==1)  push_begin(value);
-        if (k==size_) push_end(value);
-        if (k!=1 && k!=size_) {
-        Node *new_node = new Node(value);
-        Node *node = first_;
-        Node *node1= nullptr;
-        int i=1;
-        while (i!=k-1) {
-            node = node->next_;
-            i++;
+    void add_after(T value, T previous_value) {
+        if (size_ == 0) return;
+
+        if (last_->value_ == previous_value) {
+            push_last(new Node(value));
+            return;
         }
-        for (int i=0; i<2; i++) node1=node->next_;
-        node->next_=new_node;
-        new_node->next_=node1;
-        size_++;}
+
+        Node *previous = first_;
+        while (previous != nullptr) {
+            if (previous->value_ == previous_value) {
+                push_after(previous, new Node(value));
+                return;
+            }
+            previous = previous->next_;
+        }
     }
 
-    void add_after(T value, T previous) {
-        Node *node = last_;
-        if (previous==node->value_) push_end(value);
-        if (previous!=node->value_) {
-            Node *new_node = new Node(value);
-            node = first_;
-            while (node->value_!=previous){
-                node = node->next_;
-            }
-            Node *node1= node->next_;
-            node->next_=new_node;
-            new_node->next_=node1;
-            size_++;}
-    }
+    void add_before(T value, T next_value) {
+        if (size_ == 0) return;
 
-    void add_before(T value, T previous) {
-        Node *node = first_;
-        if (previous==node->value_) push_begin(value);
-        if (previous!=node->value_) {
-            Node *new_node = new Node(value);
-            node = first_;
-            while (node->next_->value_!=previous){
-                node = node->next_;
+        cout << first_->value_ << endl;
+        if (first_->value_ == next_value) {
+            push_first(new Node(value));
+            return;
+        }
+
+        Node *next = first_;
+        while (next != nullptr) {
+            if (next->value_ == next_value) {
+                push_before(next, new Node(value));
+                return;
             }
-            Node *node1= node->next_;
-            node->next_=new_node;
-            new_node->next_=node1;
-            size_++;}
+            next = next->next_;
+        }
     }
 
     T pop_begin() {
+        if (size_ == 0) throw out_of_range("List is empty");
+        if (size_ == 1) return pop_only();
+
+        return pop_first();
+    }
+
+    T pop_end() {
+        if (size_ == 0) throw out_of_range("List is empty");
+        if (size_ == 1) return pop_only();
+
+        return pop_last();
+    }
+
+    void pop(T value) {
+        if (size_ == 0) return;
         if (size_ == 1) {
-            Node *removed = last_;
-            T value = removed->value_;
-            delete removed;
-            first_ = last_ = nullptr;
-            size_ = 0;
-            return value;
-        } else {
-            Node *new_last = nullptr;
-            Node *node = first_;
-            while (node != last_) {
-                new_last = node;
-                node = node->next_;
+            if (first_->value_ == value) pop_only();
+            return;
+        }
+        if (first_->value_ == value) {
+            pop_first();
+            return;
+        }
+        if (last_->value_ == value) {
+            pop_last();
+            return;
+        }
+
+        Node* node = first_;
+        while (node != last_) {
+            if (node->value_ == value) {
+                pop(node);
+                return;
             }
-            T value = node->value_;
-            delete node;
-            new_last->next_=nullptr;
-            last_=new_last;
-            size_--;
-            return value;}
+            node = node->next_;
+        }
     }
 
     size_t size() const {
@@ -147,8 +240,6 @@ public:
         }
         cout << endl;
     }
-
-
 };
 
 #endif //LAB_666_LIST_H
